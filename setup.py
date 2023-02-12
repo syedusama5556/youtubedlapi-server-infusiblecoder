6fd45d0505d38977c33be9242e85fecc024c84cb
syedusama5556 <syedusama12345@gmail.com>
2023-02-12 22:49:49 +0500

    updated to version 3.0 and fixed instagram issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='youtubedlapi_server_infusiblecoder',
     version='3.0',
     description='An API server based on yt-dlp',
-    long_description='Get the videos from different sites using a server running yt-dlp',
+    long_description='A REST API server for getting the info for videos from different sites, powered by yt-dlp',
     author='Syed Usama Ahmad',
     author_email='syedusama5556@gmail.com',
     url='https://github.com/syedusama5556/youtubedlapi-server-infusiblecoder',
