2c0834a1372a64b7d1b7f392c8286bf0c2b6bf5d
syedusama5556 <syedusama12345@gmail.com>
2024-02-26 11:29:40 +0500

    fixed bugs

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,6 @@ RUN pip3 install -r requirements.txt
 EXPOSE 9191
 
 #ENTRYPOINT ["youtube-dl-server", "--number-processes", "1", "--host", "0.0.0.0"]
-# gunicorn -b 0.0.0.0:9191 youtubedlapi_server_infusiblecoder.app:app
+# gunicorn -w 4 -k gevent -b 0.0.0.0:9191 youtubedlapi_server_infusiblecoder.app:app
 
 ENTRYPOINT ["gunicorn", "-w","4","-k","gevent","-b", "0.0.0.0:9191", "youtubedlapi_server_infusiblecoder.app:app"]
--- setup.py
+++ setup.py
@@ -4,38 +4,41 @@ from setuptools import setup
 
 # read the contents of your README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description_txt = (this_directory / "README.md").read_text()
 
 setup(
-    name='youtubedlapi_server_infusiblecoder',
-    version='3.7.5',
-    description='An API server based on yt-dlp',
+    name="youtubedlapi_server_infusiblecoder",
+    version="3.7.6",
+    description="An API server based on yt-dlp",
     long_description=long_description_txt,
-    long_description_content_type='text/markdown',
-    author='Syed Usama Ahmad',
-    author_email='syedusama5556@gmail.com',
-    url='https://github.com/syedusama5556/youtubedlapi-server-infusiblecoder',
-    packages=['youtubedlapi_server_infusiblecoder'],
+    long_description_content_type="text/markdown",
+    author="Syed Usama Ahmad",
+    author_email="syedusama5556@gmail.com",
+    url="https://github.com/syedusama5556/youtubedlapi-server-infusiblecoder",
+    packages=["youtubedlapi_server_infusiblecoder"],
     entry_points={
-        'console_scripts': [
-            'youtubedlapi-server-infusiblecoder = youtubedlapi_server_infusiblecoder.server:main',
+        "console_scripts": [
+            "youtubedlapi-server-infusiblecoder = youtubedlapi_server_infusiblecoder.server:main",
         ],
     },
-
     install_requires=[
-        'Flask',
-        'yt_dlp >= 2023.12.30',
+        "Flask",
+        "yt_dlp >= 2023.12.30",
+        "Flask-Caching",
+        "flask_caching",
+        "gevent",
+        "gunicorn",
     ],
-
     classifiers=[
-        'Topic :: Multimedia :: Video',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'License :: Public Domain',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        "Topic :: Multimedia :: Video",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: Public Domain",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
     ],
 )
